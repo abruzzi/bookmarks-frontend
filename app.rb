b6860fdfa61c788621043f2188be1000f81ff62a
Qiu Juntao <juntao.qiu@gmail.com>
2015-06-22 15:05:35 +1000

    now we are working on mocks

--- app.rb
+++ app.rb
@@ -4,3 +4,7 @@ get '/' do
     File.open('public/index.html').read
 end
 
+get '/api/feeds' do
+	content_type 'application/json'
+	File.open('mocks/feeds.json').read
+end
--- public/script/app.js
+++ public/script/app.js
@@ -3,29 +3,10 @@ var $ = require('jquery');
 var FeedListView = require('./feed-list-view.js');
 
 $(function() {
-	var feeds = [
-	    {
-	        "id": 1,
-	        "url": "http://abruzzi.github.com/2015/03/list-comprehension-in-python/",
-	        "title": "Python中的 list comprehension 以及 generator",
-	        "publicDate": new Date()
-	    },
-	    {
-	        "id": 2,
-	        "url": "http://abruzzi.github.com/2015/03/build-monitor-script-based-on-inotify/",
-	        "title": "使用inotify/fswatch构建自动监控脚本",
-	        "publicDate": new Date()
-	    },
-	    {
-	        "id": 3,
-	        "url": "http://abruzzi.github.com/2015/02/build-sample-application-by-using-underscore-and-jquery/",
-	        "title": "使用underscore.js构建前端应用",
-	        "publicDate": new Date()
-	    }
-	];
-
-	var feedListView = new FeedListView(feeds);
-	$('.container').append(feedListView.render());
+	$.get('/api/feeds').done(function(feeds) {
+		var feedListView = new FeedListView(feeds);
+		$('.container').append(feedListView.render());
+	});
 });
 },{"./feed-list-view.js":2,"jquery":15}],2:[function(require,module,exports){
 var $ = require('jquery');
